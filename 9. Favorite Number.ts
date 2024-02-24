f061f99c05831db0b06ce4081ff511cb6d00466a
kiswaafaq <kiswaafaque14@gmail.com>
2024-02-23 22:28:36 -0800

    first commit

---
+++ 9. Favorite Number.ts
@@ -0,0 +1,7 @@
+
+
+const Favourite_Number : number=6;
+
+const message: string = 'Here is your favourite number ' + Favourite_Number ;
+
+console.log(message);
---
+++ Favorite_Number.js
@@ -0,0 +1,4 @@
+"use strict";
+const Favourite_Number = 6;
+const message = 'Here is your favourite number ' + Favourite_Number;
+console.log(message);
